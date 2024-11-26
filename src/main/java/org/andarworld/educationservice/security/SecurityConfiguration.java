a82b4a1b47b639386362ead14901a3c849093382
Adrian <andarworld@yandex.ru>
2024-11-26 21:18:20 +0400

    commit

--- src/main/java/org/andarworld/educationservice/security/SecurityConfiguration.java
+++ src/main/java/org/andarworld/educationservice/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package org.andarworld.educationservice.security;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -15,18 +16,22 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 @Slf4j
+@RefreshScope
 public class SecurityConfiguration {
-    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    @Value("${spring.main.oauth2.resourceserver.jwt.jwk-set-uri}")
     private String ISSUER_JWK;
 
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    @Value("${spring.main.oauth2.resourceserver.jwt.issuer-uri}")
     private String ISSUER_URI;
 
     @Bean
